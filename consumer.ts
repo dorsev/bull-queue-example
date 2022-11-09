f4d00c81559edd84c73ec07b59be979852184312
dorsev <dor.sever@salto.io>
2022-11-09 15:11:49 +0200

    f

--- consumer.ts
+++ consumer.ts
@@ -1,46 +1,64 @@
-import Bull, { Job, DoneCallback } from 'bull'
+import Bull, { Job, DoneCallback, Queue } from 'bull'
 import { Operation } from './Operation';
 import process from 'process'
 
-const CONCURRENT_PROCESSING = 10
-const JOB_TIMEOUT_MILI = 30 * 60 * 1000
 const LOCK_DURATION_TIME = 2 * 60 * 1000
 
-const myFirstQueue = new Bull('Operations-queue6', { 
+const myFirstQueue = new Bull('Operations-queue16', { 
     settings: {
         lockDuration: LOCK_DURATION_TIME,
+        maxStalledCount: 0,
     },
     defaultJobOptions:
     {
-         timeout: JOB_TIMEOUT_MILI,
+        //  timeout: JOB_TIMEOUT_MILI,
          removeOnComplete: true,
          removeOnFail: true
     } });
-
-
+myFirstQueue.on('completed', (job) => {
+    const totalTimeTook = job.timestamp
+    console.info('I am here %o', { start: job.timestamp, processTime: job.processedOn, finished: job.finishedOn})
+})
+let hasProcessedJob = false
 const consumer = (job: Job, done: DoneCallback): void => { 
     const data: Operation = job.data
+    if (hasProcessedJob) {
+        return
+    }
+    hasProcessedJob = true
+    myFirstQueue.close(false)
     processJob(data).then(x => {
         done()
     }).catch(e => done(e))
-    .then(x => { 
-        console.info('Game over');
-         process.exit(0)
+    .then(async x => { 
+        console.info('Game over.. waiting for 3 seconds');
+        await new Promise(resolve => setTimeout(resolve, 3000));
+        console.info('wait is over');
+        myFirstQueue.close(true)
+        process.exit(1)
     })
+}
+const printStatus = async (bullQueue: Queue<any>): Promise<void> => {
+    console.info('myFirstQueue is %o', { active: myFirstQueue.getActive() })
+    const waitingCount = myFirstQueue.getWaitingCount()
+    const allInfoCount = myFirstQueue.getJobCounts()
+    console.info('I am here %o', { waitingCount, allInfoCount })
+    await new Promise(resolve => setTimeout(resolve, 2000));
+    return printStatus(bullQueue)
 }
 
 const consumeAndDie = async () : Promise<void> => {
     console.info('I am up and running ' + process.pid)
+    
     myFirstQueue.process(1, consumer).catch(e => {
         console.info('failed to define processing function for operation queue %s, with error %o', process.pid, e)
-      })
-    console.info('I died')
+    })
 
 }
 
 const processJob = async (op: Operation): Promise<void> => {
     console.info('Im working on %o', { id: op.id, data: op.operationData })
-    await new Promise(resolve => setTimeout(resolve, 1000 * 50));
+    await new Promise(resolve => setTimeout(resolve, 100*1000))
     console.info('done working on %o', { id: op.id, data: op.operationData })
 }
 
--- producer.ts
+++ producer.ts
@@ -1,7 +1,7 @@
 import Bull, { Job, DoneCallback } from 'bull'
 import { Operation } from './Operation';
 
-const operationsQueue = new Bull('Operations-queue6');
+const operationsQueue = new Bull('Operations-queue16');
 const ops = Array(3).fill(0).map((x, i) => {
     return ({ id: i, operationData: `working on ${i}`} as Operation)
 })
@@ -10,7 +10,8 @@ const produce = async () : Promise<void> => {
         console.info('IM adding a task %o', { op })
         const job = await operationsQueue.add(op)
         console.info('finished adding a task %o', { job: job.data })
-
+        await job.finished()
+        console.info('Job  with id %o just ended', { job: job.data })
     })
 }
 
